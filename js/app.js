c0272eaded15ab7d4e6bc5968a25f88e8e4d26b5
thakursaurabh1998 <thakursaurabh1998@gmail.com>
2018-03-12 02:18:47 +0530

    Github feed added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 /*jshint esversion:6*/
+// typed.js by Matt Boldt
+// https://mattboldt.com/
 const $typedText = $('.typed-element').html();
 const elem = $('.typed');
 let typed = new Typed('#ok', {
@@ -8,8 +10,17 @@ let typed = new Typed('#ok', {
 	// loop: true,
 });
 
+// Github Activity feed by Casey Scarborough
+// https://github.com/caseyscarborough/github-activity
+GitHubActivity.feed({
+	username: "thakursaurabh1998",
+	// repository: "your-repo", // optional
+	selector: "#feed",
+	limit: 10 // optional
+});
+
 /* Smooth Scroll */
-const sections=4;
+const sections=5;
 for(let i=1;i<=sections;i++){
 	$(`#scroll${i}`).click(()=>{
 		$('html, body').animate({
@@ -17,3 +28,18 @@ for(let i=1;i<=sections;i++){
 	    }, 1000);
 	});
 }
+
+
+
+// touch swipe of the jquery carousel by Andrea Rufo
+// https://codepen.io/andrearufo/pen/rVWpyE
+$(document).ready(function() {
+	// $('#fullpage').fullpage();
+	$(".carousel").swipe({
+	  swipe: function(event, direction, distance, duration, fingerCount, fingerData) {
+	    if (direction == 'left') $(this).carousel('next');
+	    if (direction == 'right') $(this).carousel('prev');
+	  },
+	  allowPageScroll:"vertical"
+	});
+});
